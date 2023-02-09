42fd4cf9375022e116f85f46f3bf7d19c1634a6d
ab-lopez <bella.abenoja95@gmail.com>
2023-02-09 16:57:33 -0500

    Particles w Personalities Update
    yayyyyyyy we're done! i added comments yayyyy. also a gradient background. i combined my classes. also i cited sources. xoxo love and peace excited for the next project B)

--- Particles With Personalities/README.md
+++ Particles With Personalities/README.md
@@ -1,10 +1,36 @@
 # Particles w/ Personalities
 
+Title: Triggering Dissociation
+Abigail Lopez 40203553
 Prompt: Lonely and Sad
 
-Initial thoughts: tears slowly dripping down. Droplets that ripple outward. A tumultuous ocean. 
+Initial brainstorming thoughts: tears slowly dripping down. Droplets that ripple outward. A tumultuous ocean. 
 
 There's a bigger particle that is "crying", slow droplets that drip down and eventually fall to the "ocean" of particles below. Each drop causes a huge change in the ocean, making a tiny addition the cause of a huge impact.
 
 It's interactive, but it's not. They are lonely and sad. You can't help them. You can never help them. There's a quiet sniffling in the background. Heavy breathing. They sound close to tears. Everytime you click on them, the sound gets worse, the tears fall quicker. You can't help them. You can never help them. You will only make it worse. 
+
+------------------------------------------------------------------------------
+
+My prompt was "lonely and sad". I really wanted to emulate
+the feeling of being lonely and sad and full of despair and
+really capture what it feels like to dissociate, when everything
+feels so bleak and meaningless.
+
+For me, images of a relentless ocean, dripping water (my friend
+brought up the idea of Chinese water torture, which I had never
+considered but I think it's a cool idea to mention here), and the
+static in your head as you feel worse and worse and worse.
+
+Took sounds from:
+- https://www.youtube.com/watch?v=pWhjBUheoyo
+- https://www.youtube.com/watch?v=hDKoBxZbwoo
+
+Code from a variety of places:
+- https://editor.p5js.org/Jaemi13/sketches/gAS-FB5Sx, to help me figure out gradients
+- https://www.youtube.com/watch?v=Mg9Alcygelc and https://www.youtube.com/watch?v=yHDPZdjHQU4
+  to help with coding physics
+
+Also got some help with displaying the gradients from chatGPT. 
+Thank you AI for taking over the world.
 
--- Particles With Personalities/js/script.js
+++ Particles With Personalities/js/script.js
@@ -1,29 +1,50 @@
 /**
-Title of Project
-Author Name
+Triggering Dissociation
+Abigail Lopez 40203553
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+My prompt was "lonely and sad". I really wanted to emulate
+the feeling of being lonely and sad and full of despair and
+really capture what it feels like to dissociate, when everything
+feels so bleak and meaningless.
+
+For me, images of a relentless ocean, dripping water (my friend
+brought up the idea of Chinese water torture, which I had never
+considered but I think it's a cool idea to mention here), and the
+static in your head as you feel worse and worse and worse.
+
+Took sounds from:
+- https://www.youtube.com/watch?v=pWhjBUheoyo
+- https://www.youtube.com/watch?v=hDKoBxZbwoo
+
+Code from a variety of places:
+- https://editor.p5js.org/Jaemi13/sketches/gAS-FB5Sx, to help me figure out gradients
+- https://www.youtube.com/watch?v=Mg9Alcygelc and https://www.youtube.com/watch?v=yHDPZdjHQU4
+  to help with coding physics
+
+Also got some help with displaying the gradients from chatGPT. 
+Thank you AI for taking over the world.
 */
 
 "use strict";
 
 let ocean = []; // new array
 let droplets = [];
-let staticSound;
-let stormSound;
+let staticSound, stormSound; // declare variables for sounds
 
 let oceanInitial = 500; // how many particles initially, will add to it
 
-let reverb = 0;
+let reverb = 0; // declare variable for noise reverb
+
+let c1, c2; // colour variables for background
+let backgroundColor;
 
 
 /**
 Description of preload
 */
 function preload() {
-    staticSound = loadSound('assets/sounds/static.mp3');
-    stormSound = loadSound('assets/sounds/storm.mp3');
+    staticSound = loadSound('assets/sounds/static.mp3'); // load sounds, from https://www.youtube.com/watch?v=pWhjBUheoyo
+    stormSound = loadSound('assets/sounds/storm.mp3'); // from https://www.youtube.com/watch?v=hDKoBxZbwoo
 }
 
 
@@ -33,9 +54,9 @@ Description of setup
 function setup() {
     createCanvas(400, 400); // create canvas!
     for(let i = 0; i < oceanInitial; i++){   // for loop... create 200 water particles for ocean
-        ocean[i] = new WaterParticle(random(5, 390), random(250, 390), random(10,100));
+        ocean[i] = new WaterParticle(random(5, 390), random(250, 390), random(10,100), 0);
     } 
-    makeNewGuys();
+    makeNewGuys(); // call function to make a droplet
 }
 
 
@@ -43,98 +64,88 @@ function setup() {
 Description of draw()
 */
 function draw() {
-    background(100);
-
-    displayHelp();
+    createGradient();
+    image(backgroundColor, 0, 0);
 
     for(let i = 0; i < oceanInitial; i++){   // for all water particles...
         ocean[i].display(); // display them
-       // ocean[i].drop();    // make em fall
-       ocean[i].physicsVid();
-    }
-
-    for(let i = 0; i < droplets.length; i++){   // for all water particles...
-        droplets[i].display(); // display them
-        droplets[i].physicsVid();
+       ocean[i].physicsVid(); // function to make particles bounce off walls/bottom of canvas
     }
 
 }
 
+function createGradient(){ // code from https://editor.p5js.org/Jaemi13/sketches/gAS-FB5Sx
+                            // i also got coding help from chatGPT. i was having trouble finding code
+                            // that would help me copy and redraw a canvas. whoops :P at least now i know! :D
+    let c1 = color(100); // grey
+    let c2 = color(255); // white
+
+   backgroundColor = createGraphics(width, height);
+    for(let y=0; y<height; y++){
+        let n = map(y,0,height,0,1);
+        let newc = lerpColor(c1,c2,n);
+        stroke(newc);
+        line(0,y,width, y);
+      }
+}
+
 class WaterParticle {
-    constructor(x, y, size) {
+    constructor(x, y, size, type) {
         this.x = x; // random x coordinate
         this.y = y; // random y coordinate
-        this.size = size
+        this.size = size // size
+        this.type = type; // number, determines if particle is "ocean" (0) or "droplet" (1)
 
-        this.xVel = random(-2,2);
+        this.xVel = random(-2,2); // velocities
         this.yVel = 0;
         this.xAcc = 0;
         this.yAcc = 0.05;
     }
     display() {
         fill(0); // fill black
-        ellipse(this.x, this.y, this.size);
+        noStroke();
+        ellipse(this.x, this.y, this.size); // create/display particles
     }
 
-    physicsVid(){ // like the falling!
+    physicsVid(){ // like the falling! from https://www.youtube.com/watch?v=Mg9Alcygelc
+                                        // also from https://www.youtube.com/watch?v=yHDPZdjHQU4
+           
+            if(this.type == 1){ // if "droplet", no bouncing off walls, therefore xVelocity can be 0 to stay straight
+                this.xVel = 0;
+            }
 
             this.x += this.xVel;
             this.y += this.yVel;
             this.xVel += this.xAcc;
             this.yVel += this.yAcc; 
 
-            if(this.y >= 400){ // bouncing off floor
-                this.yVel *= -0.95;
+            if(this.y >= 400 && this.type == 0){ // bouncing off floor, but only if type is "ocean"
+                this.yVel *= -0.99; // reverse the velocity when it bounces off floor, less than -1
+                                    // so that it slowly "calms" down. exponential decrease? or something similar
             }
 
             if(this.x >= 390 || this.x <= 5){ // bounce off walls
-                this.xVel *= -1;
+                this.xVel *= -1; // no exponential decrease
             }
-    }
-}
-
-class WaterDroplet{
-    constructor(x, y, size) {
-        this.x = x; // random x coordinate
-        this.y = y; // random y coordinate
-        this.size = size
-
-        this.xVel = 0;
-        this.yVel = 0;
-        this.xAcc = 0;
-        this.yAcc = 0.05;
-    }
-  
-    display() {
-        fill(0); // fill black
-        ellipse(this.x, this.y, this.size);
-    }
-
-    physicsVid(){ // like the falling!
-
-        this.xVel = 0;
-
-        this.x += this.xVel;
-        this.y += this.yVel;
-        this.xVel += this.xAcc;
-        this.yVel += this.yAcc; 
     }
 }
 
 function makeNewGuys() {
-    drippingWater();
-    setInterval(drippingWater, ((random(2, 20))*1000));
+    drippingWater(); // call function once to make water go
+    setInterval(drippingWater, ((random(2,20))*1000)); // timer to start dripping water
+                                                       // runs once every 2-20 seconds to make it irregular 
 
 }
 
 function drippingWater() {
-    let newParticle = new WaterDroplet(200, 0, 10);
-    droplets.push(newParticle);
+    let newParticle = new WaterParticle(200, 0, 10, 1); // create new water droplet particle
+    ocean.push(newParticle); // push the array
+    oceanInitial++; // increase variables storing number of ocean initial on screen to make droplet visible
 }
 
 function keyPressed(){
-    staticSound.play();
-    staticSound.setVolume(0.01);
+    staticSound.play(); // play, set the volume to a certain level, loop
+    staticSound.setVolume(0.01); // google doesn't let sound auto play so it has to be linked to a user function
     staticSound.loop();
 
     stormSound.play();
@@ -143,16 +154,10 @@ function keyPressed(){
 }
 
 function mousePressed(){
-    reverb += 0.01;
-    staticSound.amp(reverb);
+    reverb += 0.01; // everytime the mouse is clicked, reverb goes up
+    staticSound.amp(reverb); // the static noises get louder
 
-    if(reverb >= 0.05){
+    if(reverb >= 0.05){ // at a certain point, the ocean noises will begin to decrease so the static takes precedence
     stormSound.setVolume(0.03 - reverb);
     }
-}
-
-function displayHelp(){
-    textSize(32);
-    fill(130);
-    text('help. help?', 130, 250);
 }
